397c2f2dcf519915a2ec590fc9dc63b0d7213fae
Seif Eldin Sameh <seiffsameh00@gmail.com>
2024-01-08 16:56:27 +0200

    Enggam Website

--- app/(Home Page)/Home-Page/page.tsx
+++ app/(Home Page)/Home-Page/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import Swal from "sweetalert2";
 import dynamic from "next/dynamic";
 
@@ -15,45 +15,41 @@ const HeaderHome = dynamic(() => import("@/lib/HeaderHome"));
 const Cta = dynamic(() => import("@/common/Cta"));
 
 export default function page() {
-  const [hasShownReminder, setHasShownReminder] = useState(false);
   useEffect(() => {
-    if (!hasShownReminder) {
-      const timeoutId = setTimeout(() => {
-        Swal.fire({
-          title: "Important Reminder: Secure Your Account with Sign Out",
-          html: `
-          <h4 style="text-align: left;">For your security, we automatically log you out of ENGGAM after <strong>5 minutes</strong> of inactivity. This helps protect your account and sensitive information from unauthorized access, especially if you're using a shared device or stepping away from your computer.</h4>
-          <br/>
-          <p style="text-align: left;">        
-          To ensure a seamless experience:
-          <br/><br/>
-          
-              - Always sign out before closing your browser window or leaving the website.<br/>
-              - If you haven't signed out and attempt to access the site again within <strong>5 minutes</strong>, you'll be prompted to log in again.<br/>
-          
-          Thank you for your cooperation in keeping your account safe.
-          </p>
-          `,
-          imageUrl: "https://i.imgur.com/L9ON2E7.png",
-          backdrop: `
-          rgba(0, 86, 179,0.4)
-          left top
-          no-repeat
+    const timeoutId = setTimeout(() => {
+      Swal.fire({
+        title: "Important Reminder: Secure Your Account with Sign Out",
+        html: `
+        <h4 style="text-align: left;">For your security, we automatically log you out of ENGGAM after <strong>5 minutes</strong> of inactivity. This helps protect your account and sensitive information from unauthorized access, especially if you're using a shared device or stepping away from your computer.</h4>
+        <br/>
+        <p style="text-align: left;">        
+        To ensure a seamless experience:
+        <br/><br/>
+        
+            - Always sign out before closing your browser window or leaving the website.<br/>
+            - If you haven't signed out and attempt to access the site again within <strong>5 minutes</strong>, you'll be prompted to log in again.<br/>
+        
+        Thank you for your cooperation in keeping your account safe.
+        </p>
         `,
-          imageWidth: 100,
-          imageHeight: 100,
-          imageAlt: "Enggam",
-          confirmButtonColor: "#0056b3",
-          confirmButtonText: "Okay, Got it!",
-        });
-        setHasShownReminder(true);
-      }, 5000);
+        imageUrl: "https://i.imgur.com/L9ON2E7.png",
+        backdrop: `
+        rgba(0, 86, 179,0.4)
+        left top
+        no-repeat
+      `,
+        imageWidth: 100,
+        imageHeight: 100,
+        imageAlt: "Enggam",
+        confirmButtonColor: "#0056b3",
+        confirmButtonText: "Okay, Got it!",
+      });
+    }, 5000);
 
-      return () => {
-        clearTimeout(timeoutId);
-      };
-    }
-  }, [hasShownReminder]);
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, []);
   return (
     <main className="home-page">
       <HeaderHome />
