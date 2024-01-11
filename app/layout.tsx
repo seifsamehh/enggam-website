a6fec14e96a00cf71353bd8c1f20997a4107bd19
Seif Eldin Sameh <seiffsameh00@gmail.com>
2024-01-11 22:27:09 +0200

    Enggam Website

--- app/layout.tsx
+++ app/layout.tsx
@@ -70,6 +70,7 @@ export const metadata: Metadata = {
   manifest: "/manifest.json",
   icons: {
     icon: [
+      { url: "/app/favicon.ico", sizes: "48x48", type: "image/x-icon" },
       { url: "/icon-16x16.png", sizes: "16x16", type: "image/png" },
       { url: "/icon-32x32.png", sizes: "32x32", type: "image/png" },
       { url: "/icon-48x48.png", sizes: "48x48", type: "image/png" },
