92dbd121bfc12e0c11764742b2f40af6e50cdc1d
Seif Eldin Sameh <seiffsameh00@gmail.com>
2024-01-12 10:38:18 +0200

    Enggam Website

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import "./globals.css";
 import LoadingPage from "@/common/LoadingPage";
-import Head from "next/head";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Analytics } from "@vercel/analytics/react";
 import { Parallax } from "./parallax";
@@ -8,6 +7,8 @@ import { Providers } from "./providers";
 import type { Metadata } from "next";
 import { PT_Serif } from "next/font/google";
 import CrispChat from "@/components/CrispChat";
+import Head from "next/head";
+import Script from "next/script";
 
 const ptSerif = PT_Serif({
   subsets: ["latin"],
@@ -143,6 +144,162 @@ export const metadata: Metadata = {
   category: "Cards Store",
 };
 
+// structure data
+interface Product {
+  name: string;
+  price: number;
+  availability: string;
+  brand: string;
+  image: string;
+}
+interface WebsiteData {
+  "@context": string;
+  "@type": string;
+  name: string;
+  url: string;
+  logo: string;
+  favicon: string;
+  address: {
+    "@type": string;
+    addressRegion: string;
+    postalCode: string;
+    addressCountry: string;
+  };
+  contactPoint: {
+    "@type": string;
+    telephone: string;
+    email: string;
+  };
+  description?: string;
+  foundingDate?: string;
+  founders?: string[];
+  employees?: number;
+  industry?: string;
+  socialMedia?: {
+    twitter?: string;
+    facebook?: string;
+    instagram?: string;
+  };
+  products?: Product[];
+}
+
+const website: WebsiteData = {
+  "@context": "https://schema.org",
+  "@type": "WebSite",
+  name: "Enggam",
+  url: "https://www.enggam.com/",
+  logo: "https://i.imgur.com/3xFPJUP.jpg",
+  favicon: "https://i.imgur.com/3xFPJUP.jpg",
+  address: {
+    "@type": "PostalAddress",
+    addressRegion: "Cairo Governorate",
+    postalCode: "11835",
+    addressCountry: "Egypt",
+  },
+  contactPoint: {
+    "@type": "ContactPoint",
+    telephone: "+201014402598",
+    email: "enggam729@gmail.com",
+  },
+  description:
+    "Enggam is a Gift Card Company located in Egypt. We sell all prepaid cards like iTunes cards, google play cards, and more.",
+  foundingDate: "January 11, 2020",
+  founders: ["Ali Nasr"],
+  employees: 50,
+  industry: "Technology",
+  socialMedia: {
+    twitter: "https://twitter.com/enggam729",
+    facebook: "https://www.facebook.com/profile.php?id=100095357415380",
+    instagram: "https://www.instagram.com/enggam.co/",
+  },
+  products: [
+    {
+      name: "iTunes 50 CNY China",
+      price: 9.0,
+      availability: "In Stock",
+      brand: "Itunes",
+      image: "https://i.imgur.com/eO7Waro.png",
+    },
+    {
+      name: "Google play AUD20 Australia (AU)",
+      price: 15.96,
+      availability: "In Stock",
+      brand: "Google Play",
+      image: "https://i.imgur.com/oNyTjZP.png",
+    },
+    {
+      name: "Netflix UAE – AED 100",
+      price: 30.9,
+      availability: "In Stock",
+      brand: "Netflix",
+      image: "https://i.imgur.com/WWXMYYv.png",
+    },
+    {
+      name: "$5 Razer Gold Rixty (USA)",
+      price: 5.04,
+      availability: "In Stock",
+      brand: "Razer Gold",
+      image: "https://i.imgur.com/3Qb6mQS.png",
+    },
+    {
+      name: "PUBG 300+25 UC (Global) Code",
+      price: 4.25,
+      availability: "In Stock",
+      brand: "Pubg Mobile",
+      image: "https://i.imgur.com/PkAX5Z1.png",
+    },
+    {
+      name: "Free Fire 1080 + 108 Diamonds Pins (Garena)",
+      price: 9.99,
+      availability: "In Stock",
+      brand: "Free Fire",
+      image: "https://i.imgur.com/mNdyvHn.png",
+    },
+    {
+      name: "£10 Roblox (United Kingdom)",
+      price: 12.87,
+      availability: "In Stock",
+      brand: "Roblox",
+      image: "https://i.imgur.com/oetHu1o.png",
+    },
+    {
+      name: "Fortnite 2800 V-Bucks Card",
+      price: 28.84,
+      availability: "In Stock",
+      brand: "Fortnite",
+      image: "https://i.imgur.com/cQemSnQ.png",
+    },
+    {
+      name: "Neosurf Prepaid Card 5 EURO",
+      price: 7.21,
+      availability: "In Stock",
+      brand: "Neosurf",
+      image: "https://i.imgur.com/n6YeL8q.png",
+    },
+    {
+      name: "OBUCKS CARD USD 10",
+      price: 10.19,
+      availability: "In Stock",
+      brand: "Openbucks",
+      image: "https://i.imgur.com/7vKuzRa.png",
+    },
+    {
+      name: "AstroPay Card 20EGP",
+      price: 2.06,
+      availability: "In Stock",
+      brand: "AstroPay",
+      image: "https://i.imgur.com/XlLGVuT.png",
+    },
+    {
+      name: "500CAD Flexepin (CA)",
+      price: 462.47,
+      availability: "In Stock",
+      brand: "Flexepin",
+      image: "https://i.imgur.com/rHTa9kE.png",
+    },
+  ],
+};
+
 export default function RootLayout({
   children,
 }: {
@@ -151,9 +308,6 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <Head>
-          <link rel="icon" type="image/x-icon" href="/app/favicon.ico" />
-        </Head>
         <body className={ptSerif.className}>
           <Providers>
             <LoadingPage />
@@ -161,6 +315,10 @@ export default function RootLayout({
           </Providers>
           <CrispChat />
           <Analytics />
+          <Script
+            type="application/ld+json"
+            dangerouslySetInnerHTML={{ __html: JSON.stringify(website) }}
+          />
         </body>
       </html>
     </ClerkProvider>
