ab178edec61419b7e3de30a81e7d7cf94ac22051
Seif Eldin Sameh <seiffsameh00@gmail.com>
2024-05-26 22:26:59 +0300

    Cancel Page Updated

--- common/Cancel.tsx
+++ common/Cancel.tsx
@@ -2,8 +2,10 @@ import Link from "next/link";
 
 const Cancel = () => {
   return (
-    <section className="cancel bg-red-600 h-screen flex justify-center items-center gap-6 flex-col">
-      <h1>The order has been cancelled</h1>
+    <section className="cancel h-screen flex justify-center items-center gap-6 flex-col">
+      <h1 className="text-3xl md:text-9xl font-bold">
+        The order has been cancelled
+      </h1>
       <Link
         href="/Home-Page"
         className="bg-white text-red-600 px-4 py-2 rounded-md"
