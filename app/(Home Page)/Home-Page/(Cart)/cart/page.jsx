aef96d474c5f09e19b9972a55a2fbd6782e8a55d
Seif Eldin Sameh <seiffsameh00@gmail.com>
2024-05-27 18:41:59 +0300

    Cart Updated

--- app/(Home Page)/Home-Page/(Cart)/cart/page.jsx
+++ app/(Home Page)/Home-Page/(Cart)/cart/page.jsx
@@ -203,8 +203,8 @@ const CartPage = () => {
   };
 
   const handleSubmit = async () => {
-    const name = products.map((product) => product.name).join(", ");
     const quantity = products.map((product) => product.quantity).join(", ");
+    const name = products.map((product) => product.name).join(", ") + quantity;
     const email = customerEmail;
     const amount = paymentAmount;
     const clientName = customerName;
